e3ddc4bf2b6a86baec63888bd7e7292db6efe6a2
JeremyTKS <45128018+JeremyTKS@users.noreply.github.com>
2024-07-07 15:41:58 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    document.getElementById("bookingButton").addEventListener("click", function() {
+        window.location.href = "bookingOp.html";
+    });
+    document.getElementById("financeButton").addEventListener("click", function() {
+        window.location.href = "finance.html";
+    });
+    document.getElementById("userButton").addEventListener("click", function() {
+        window.location.href = "user.html";
+    });
+    document.getElementById("settingButton").addEventListener("click", function() {
+        window.location.href = "settings.html";
+    });
+});
